994e9e140704bb22cd3f724845c6d24306f18c1f
noamgot <noam.got@gmail.com>
2018-05-02 11:22:38 +0300

    backup

---
+++ Lesson 2/video2.py
@@ -0,0 +1,15 @@
+lst = [4,8,3,1]
+# print(max(lst))
+# print(min(lst))
+# print(sorted(lst))
+# print(lst)
+# lst = sorted(lst)
+# print(lst)
+#print(sorted([1,"hello"]))
+#print(sorted(["computer", "hello", "dog"]))
+# my_lst = [1,2,3]
+# my_str = "hello"
+# my_lst[0] = 7
+# print(my_lst)
+# my_str[0] = "H"
+#print(sum([1,2,3,4]))
